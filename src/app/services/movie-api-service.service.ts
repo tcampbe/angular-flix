dc9d10ec0d6d9145ea14ab153b2e0e6ea763085c
Jordan Woodruff <boogermanus@gmail.com>
2020-02-03 20:42:42 -0600

    test

--- src/app/services/movie-api-service.service.ts
+++ src/app/services/movie-api-service.service.ts
@@ -12,11 +12,12 @@ export class MovieApiServiceService {
   constructor(private httpClient: HttpClient) {}
 
   url = "https://api.themoviedb.org/3/";
+  api_key = '33f59a5c5ff0dcf180c32c8e4dcd5792';
 
   async get(path, options?): Promise<any> {
     return await this.httpClient
-      .get(this.url + path 
-        + "&api_key=33f59a5c5ff0dcf180c32c8e4dcd5792", 
+      .get(this.url + path
+        + `&api_key=${this.api_key}`,
         options)
       .toPromise();
   }
